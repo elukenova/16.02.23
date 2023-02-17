3cce479a50d32befecf67d1d7ed14be680336159
Aijanyl Elukenova <ajowka05@gmail.com>
2023-02-17 13:27:04 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+function myName(name, surname) {
+  console.log("My name is " + name + surname);
+}
+myName("Aijanyl ", "Elukenova");
+
+
+pi = 3.14
+function areaReg(h, r) {
+  console.log(V = 3 / 1 * pi * r ** r * h);
+}
